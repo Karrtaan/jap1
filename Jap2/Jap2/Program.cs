c9c38ff7a4f392c498f14a300935b6a63729f35f
czerasky2 <48946208+czerasky2@users.noreply.github.com>
2019-03-26 12:09:48 +0100

    Add files via upload

---
+++ Jap2/Jap2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            Console.Write("Whats your name : ");
+            name = Console.ReadLine();
+            Console.Write("Whats your age : ");
+            age = Console.ReadLine();
+            Console.WriteLine("Your name is '{0}'", name);
+            Console.WriteLine("Your age is '{0}'", age);
+        }
+
+
+
+    }
+}
